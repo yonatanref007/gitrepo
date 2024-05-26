4ef27674d61f5f9bd22c4488c5a0ce81bb95f4b5
yonatanref007 <yonatanref007@gmail.com>
2024-05-26 22:52:04 +0300

    yehonatan

--- Yonatan.java
+++ Yonatan.java
@@ -2,7 +2,7 @@ public class Yonatan{
         
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("Yonatan"+i);
+            System.out.println("Yehonatan"+i);
         }
         }
     }
